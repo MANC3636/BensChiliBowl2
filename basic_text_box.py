1c60738f2bfb53ec8842e8efb76c8f2da4d08fb7
MANC3636 <73012287+MANC3636@users.noreply.github.com>
2021-01-15 15:11:01 -0500

    Update basic_text_box.py

--- basic_text_box.py
+++ basic_text_box.py
@@ -5,14 +5,12 @@ import sys
 pg.init()
 screen=pg.display.set_mode((500,500))
 
-text="Amy"
-text1="is a great student"
+text="Jeremiah"
 
-font=pg.font.Font('freesansbold.ttf',20)
-text_surface=font.render(text, True, (55, 2, 222))
-#text_surface1=font.render(text1, True, (55, 199, 22))
-text_rect=text_surface.get_rect(center =(100, 100))
-#text_rect1=text_surface1.get_rect(center=(100, 130))
+font=pg.font.Font('freesansbold.ttf',20)#we need a font
+text_surface=font.render(text, True, (55, 2, 222))#we need a text surface
+text_rect=text_surface.get_rect(center =(100, 100))#we need a text rect
+
 
 
 running =True
@@ -22,8 +20,8 @@ while running:
             sys.exit()
 
     screen.fill((100,100,199))
-    screen.blit(text_surface, text_rect)
-    #screen.blit(text_surface1, text_rect1)
+    screen.blit(text_surface, text_rect)#we need to blit the text_surface onto the screen at a location
+    
     pg.display.flip()
 
 
